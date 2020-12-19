e087ab04dd4cdace33ff6b0611a540405eff0e4d
ChristianAnagnostou <69920522+ChristianAnagnostou@users.noreply.github.com>
2020-12-18 21:52:42 -0800

    Accessability improvements

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './Playlist.css'
-import {TrackList} from '../TrackList/TrackList';
+import React from "react";
+import "./Playlist.css";
+import { TrackList } from "../TrackList/TrackList";
 
 export class Playlist extends React.Component {
-    handlePlaylistNameChange = (e) => {
-        this.props.updatePlaylistName(e.target.value);
-    }
+  handlePlaylistNameChange = (e) => {
+    this.props.updatePlaylistName(e.target.value);
+  };
 
-    render() {
-        return (
-        <div className="Playlist" >
-            <input value={this.props.playlistName} onChange={this.handlePlaylistNameChange}/>
-            <TrackList 
-                tracks={this.props.playlist} 
-                isRemovable={true}
-                onRemove={this.props.onRemove}
-            />
-            <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Playlist">
+        <input
+          type="text"
+          value={this.props.playlistName}
+          onChange={this.handlePlaylistNameChange}
+        />
+        <TrackList tracks={this.props.playlist} isRemovable={true} onRemove={this.props.onRemove} />
+        <button className="Playlist-save" onClick={this.props.onSave}>
+          SAVE TO SPOTIFY
+        </button>
+      </div>
+    );
+  }
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import './SearchBar.css'
-
+import React from "react";
+import "./SearchBar.css";
 
 export class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {inputTerm: ''}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { inputTerm: "" };
+  }
 
-    handleTermChange = (e) => {
-        this.setState({inputTerm: e.target.value});
-    }
+  handleTermChange = (e) => {
+    this.setState({ inputTerm: e.target.value });
+  };
 
-    handleSearch = () => {
-        this.props.onSearch(this.state.inputTerm);
-    }
+  handleSearch = () => {
+    this.props.onSearch(this.state.inputTerm);
+  };
 
-    handleKeyPress = (e) => {
-        if(e.key === "Enter"){
-            this.handleSearch();
-        }
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleSearch();
     }
+  };
 
-    render() {
-        return (
-            <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
-                <button className="SearchButton" onClick={this.handleSearch}>SEARCH</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="SearchBar">
+        <input
+          type="text"
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange}
+          onChange={this.handleKeyPress}
+        />
+        <button className="SearchButton" onClick={this.handleSearch}>
+          SEARCH
+        </button>
+      </div>
+    );
+  }
 }
